33049f9c92497cd2c2e2a4db2803014d0f300150
matistark <mcornejo@miuandes.cl>
2020-05-26 16:13:52 -0400

    Paro

---
+++ Entrega 3/Clases/Actor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega_3.Clases
+{
+    class Actor
+    {
+    }
+}
